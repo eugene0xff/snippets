ed63bf67bcfa288929fffa27ab3ce3d22fa2239d
eugene0xff <true.enterprise.developer@gmail.com>
2026-07-04 21:01:51 +0300

    [r_grimm-concur_with_modern_cpp][ch 02] add the 7-th example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/07.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <thread>
+
+int main() {
+    std::cout << '\n';
+
+    std::cout << std::boolalpha << '\n';
+
+    std::cout << "hardware_concurrency() = " << std::thread::hardware_concurrency() << '\n';
+
+    std::thread t1([] { std::cout << "t1 with id = " << std::this_thread::get_id() << '\n'; });
+    std::thread t2([] { std::cout << "t2 with id = " << std::this_thread::get_id() << '\n'; });
+
+    std::cout << '\n';
+
+    std::cout << "FROM MAIN: id of t1 " << t1.get_id() << '\n';
+    std::cout << "FROM MAIN: id of t2 " << t2.get_id() << '\n';
+
+    std::cout << '\n';
+
+    std::swap(t1, t2);
+
+    std::cout << '\n';
+
+    std::cout << "FROM MAIN: id of t1 " << t1.get_id() << '\n';
+    std::cout << "FROM MAIN: id of t2 " << t2.get_id() << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "FROM MAIN: id of main = " << std::this_thread::get_id() << '\n';
+
+    std::cout << '\n';
+
+    std::cout << "t1.joinable(): " << t1.joinable() << '\n';
+    std::cout << "t2.joinable(): " << t2.joinable() << '\n';
+
+    std::cout << '\n';
+
+    t1.join();
+    t2.join();
+
+    std::cout << '\n';
+
+    std::cout << "t1.joinable(): " << t1.joinable() << '\n';
+    std::cout << "t2.joinable(): " << t2.joinable() << '\n';
+
+    std::cout << '\n';
+}
