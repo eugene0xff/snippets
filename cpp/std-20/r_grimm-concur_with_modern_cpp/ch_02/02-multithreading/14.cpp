8318266558e0edb72254e0c01b19fa4958940ce4
eugene0xff <true.enterprise.developer@gmail.com>
2026-07-11 22:52:23 +0300

    [r_grimm-concur_with_modern_cpp][ch 02] add the 14-th example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/14.cpp
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+
+struct CriticalData {
+    std::mutex mut;
+};
+
+void deadLock(CriticalData& a, CriticalData& b) {
+    std::cout << "thread: " << std::this_thread::get_id() << " first mutex" << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    std::cout << "  thread: " << std::this_thread::get_id() << " second mutex" << '\n';
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+    std::cout << "    thread: " << std::this_thread::get_id() << " get both mutex" << '\n';
+    std::scoped_lock lock(a.mut, b.mut);
+}
+
+
+int main() {
+    std::cout << '\n';
+
+    CriticalData c1;
+    CriticalData c2;
+    std::thread t1([&] { deadLock(c1, c2); });
+    std::thread t2([&] { deadLock(c2, c1); });
+
+    t1.join();
+    t2.join();
+
+    std::cout << '\n';
+}
