7099feabf2bd7f9bd3e2c0595232d14dca51301f
eugene0xff <true.enterprise.developer@gmail.com>
2026-07-16 23:13:03 +0400

    [r_grimm-concur_with_modern_cpp][ch 02] add the 19-the example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/19.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <mutex>
+
+std::mutex coutMutex;
+
+thread_local std::string s("hello from ");
+
+void addThreadLocal(std::string const& s2) {
+    s += s2;
+
+    std::lock_guard<std::mutex> guard(coutMutex);
+    std::cout << s << '\n';
+    std::cout << "&s: " << &s << '\n';
+    std::cout << '\n';
+}
+
+
+int main() {
+    std::cout << '\n';
+
+    std::thread t1(addThreadLocal, "t1");
+    std::thread t2(addThreadLocal, "t2");
+    std::thread t3(addThreadLocal, "t3");
+    std::thread t4(addThreadLocal, "t4");
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    std::cout << s << '\n';
+    std::cout << "&s: " << &s << '\n';
+
+    std::cout << '\n';
+}
