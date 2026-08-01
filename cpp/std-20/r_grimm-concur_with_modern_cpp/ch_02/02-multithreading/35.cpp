a611cf175a05b093a977ae3b1734e5440f6aab3e
eugene0xff <true.enterprise.developer@gmail.com>
2026-08-01 22:39:57 +0400

    [r_grimm-concur_with_modern_cpp][ch 02] add the 35-th example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/35.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <latch>
+#include <mutex>
+#include <thread>
+
+
+std::latch workDone(6);
+std::mutex coutMutex;
+
+void synchronizedOut(std::string const& s) {
+	std::lock_guard<std::mutex> _(coutMutex);
+	std::cout << s;
+}
+
+class Worker {
+public:
+	Worker(std::string const& name) : name_(name) {};
+
+	void operator() () {
+		synchronizedOut(name_ + ": " + "Work done!\n");
+		workDone.arrive_and_wait();
+		synchronizedOut(name_ + ": " + "See you tomorrow!\n");
+	}
+
+private:
+	std::string const name_;
+};
+
+
+int main() {
+	std::cout << '\n';
+
+	Worker herb("  Herb");
+	std::thread herbWork(herb);
+
+	Worker scott("    Scott");
+	std::thread scottWork(scott);
+
+	Worker bjarne("      Bjarne");
+	std::thread bjarneWork(bjarne);
+
+	Worker andrei("        Andrei");
+	std::thread andreiWork(andrei);
+
+	Worker andrew("          Andrew");
+	std::thread andrewWork(andrew);
+
+	Worker david("             David");
+	std::thread davidWork(david);
+
+	herbWork.join();
+	scottWork.join();
+	bjarneWork.join();
+	andreiWork.join();
+	andrewWork.join();
+	davidWork.join();
+
+	std::cout << '\n';
+}
