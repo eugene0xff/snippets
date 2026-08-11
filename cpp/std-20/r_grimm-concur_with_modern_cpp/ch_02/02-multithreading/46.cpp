17d7f739be352cf5ef0f3908979d19bc5fc608af
eugene0xff <true.enterprise.developer@gmail.com>
2026-08-11 22:53:29 +0400

    [r_grimm-concur_with_modern_cpp][ch 02] add the 46-th example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/46.cpp
@@ -0,0 +1,58 @@
+#include <future>
+#include <iostream>
+#include <thread>
+#include <utility>
+
+
+std::mutex g_coutMutex;
+
+struct Div {
+	void operator()(std::promise<int>&& intPromise, int const a, int const b) {
+		intPromise.set_value(a / b);
+	}
+};
+
+struct Requestor {
+	void operator()(std::shared_future<int> aSharedFuture) {
+		std::lock_guard<std::mutex> _(g_coutMutex);
+		std::cout << "threadId(" << std::this_thread::get_id() << "): ";
+		std::cout << "20 / 10 = " << aSharedFuture.get() << '\n';
+	}
+};
+
+
+int main() {
+	std::cout << '\n';
+
+	std::promise<int> divPromise;
+
+	std::future<int> divResult = divPromise.get_future();
+
+	std::cout << "divResult.valid(): " << divResult.valid() << '\n';
+
+	Div div;
+	std::thread divThread(div, std::move(divPromise), 20, 10);
+
+	std::cout << "divResult.valid(): " << divResult.valid() << '\n' << '\n';
+
+	std::shared_future<int> sharedResult = divResult.share();
+
+	std::cout << "divResult.valid(): " << divResult.valid() << '\n' << '\n';
+
+	Requestor req;
+	std::thread sharedThread1(req, sharedResult);
+	std::thread sharedThread2(req, sharedResult);
+	std::thread sharedThread3(req, sharedResult);
+	std::thread sharedThread4(req, sharedResult);
+	std::thread sharedThread5(req, sharedResult);
+
+	divThread.join();
+
+	sharedThread1.join();
+	sharedThread2.join();
+	sharedThread3.join();
+	sharedThread4.join();
+	sharedThread5.join();
+
+	std::cout << '\n';
+}
