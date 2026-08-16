66870879fd160cacaa20746565a4414b856e0c59
eugene0xff <true.enterprise.developer@gmail.com>
2026-08-16 22:59:09 +0400

    [r_grimm-concur_with_modern_cpp][ch 02] add the 51-st example

---
+++ cpp/std-20/r_grimm-concur_with_modern_cpp/ch_02/02-multithreading/51.cpp
@@ -0,0 +1,49 @@
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+
+std::mutex g_coutMutex;
+
+class Worker {
+public:
+	Worker(std::string const& name) : name_(name) {};
+
+	void operator()() {
+		for (int i = 1; i <= 3; ++i) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+			std::lock_guard<std::mutex> _(g_coutMutex);
+			std::cout << name_ << ": " << "Work " << i << " done !!!\n";
+		}
+	}
+
+private:
+	std::string const name_;
+};
+
+
+int main() {
+	std::cout << '\n';
+
+	std::cout << "Boss: Let's start working." << "\n\n";
+
+	std::thread herb = std::thread(Worker("Herb"));
+	std::thread andrei = std::thread(Worker("  Andrei"));
+	std::thread scott = std::thread(Worker("    Scott"));
+	std::thread bjarne = std::thread(Worker("      Bjarne"));
+	std::thread bart = std::thread(Worker("        Bart"));
+	std::thread jenne = std::thread(Worker("          Jenne"));
+
+	herb.join();
+	andrei.join();
+	scott.join();
+	bjarne.join();
+	bart.join();
+	jenne.join();
+
+	std::cout << '\n' << "Boss: Let's go home." << '\n';
+
+	std::cout << '\n';
+}
